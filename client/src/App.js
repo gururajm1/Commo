9d583f783913676ec8e916844d9ce3b480208536
Gururaj2004 <guruguru6631@gmail.com>
2024-03-27 18:04:05 +0530

    Finished upto frontend part

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import { useEffect } from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom'
+import Home from "./scenes/home/Home"
+import ItemDetails from '../src/scenes/itemDetails/ItemDetails'
+import Checkout from '../src/scenes/checkout/Checkout'
+import Conformation from '../src/scenes/checkout/Conformation'
+import Navbar from './scenes/global/Navbar'
+import CartMenu from './scenes/global/CartMenu'
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null;
+}
+
+function App() {
+  return <div className="app">
+    <BrowserRouter>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="item/:itemId" element={<ItemDetails />} />
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="checkout/success" element={<Conformation />} />
+        </Routes>
+        <CartMenu />
+    </BrowserRouter>
+  </div>;
+}
+
+export default App;
---
+++ client/src/scenes/home/Home.jsx
@@ -0,0 +1,11 @@
+import MainCarousel from "./MainCarousel";
+import ShoppingList from './ShoppingList'
+
+const Home = () => {
+    return <div>
+        <MainCarousel />
+        <ShoppingList />
+    </div>
+}
+
+export default Home;
